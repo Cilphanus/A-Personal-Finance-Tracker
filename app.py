7b241b2f6dc711cf95e99f2c808f5fdc9dd09b63
AishaAgarwal <107138192+AishaAgarwal@users.noreply.github.com>
2023-06-01 23:04:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 import json
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify
 import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report
 
 app = Flask(__name__)
 
@@ -68,63 +64,28 @@ def calculate_monthly_summary(df):
 
 
 # Endpoint for training and making predictions
-@app.route('/predict', methods=['POST'])
-def predict():
-    # Get the data from the request
-    data = request.json
-    
-    # Load the dataset
-    df = pd.DataFrame(data)
-    
-    # Preprocess the data
-    X = pd.get_dummies(df[['Category', 'Transaction_Amount', 'Transaction_Date']])
-    y = df['Transaction_Type']
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-    # Split the data into training and test sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-    # Select a machine learning algorithm (Random Forest in this example)
-    model = RandomForestClassifier()
-
-    # Train the model
-    model.fit(X_train, y_train)
-
-    # Make predictions
-    y_pred = model.predict(X_test)
-
-    # Evaluate the model
-    report = classification_report(y_test, y_pred)
-    
-    # Return the classification report as a JSON response
-    return jsonify({'classification_report': report})
-
-@app.route('/api/categorized-transactions', methods=['POST'])
-def get_categorized_transactions():
-    data = request.json
-    
-    # Load the dataset
-    df = pd.DataFrame(data)
-    
-    # Categorize the transactions
-    categorized_transactions = categorize_transactions(df)
-    
-    # Return the categorized transactions as a JSON response
-    return jsonify(categorized_transactions)
-
-# API endpoint for generating the monthly summary
 @app.route('/api/monthly-summary', methods=['POST'])
 def generate_monthly_summary():
     data = request.json
     
     # Load the dataset
     df = pd.DataFrame(data)
-    
-    # Generate the monthly summary
+
+    categorized_transactions = categorize_transactions(df)
     monthly_summary = calculate_monthly_summary(df)
     
+    summary_data = {
+        'categorized_transactions': categorized_transactions,
+        'monthly_summary': monthly_summary
+    }
+    
     # Return the monthly summary as a JSON response
-    return jsonify(monthly_summary)
+    return jsonify(summary_data)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True)
